2e9153a2b7440086ef61edfb41ee8ebc5c5e96df
dipak022 <dipakd3ebnath4002@gmail.com>
2021-04-24 11:06:54 +0600

    Complate ecomarce

---
+++ app/Http/Controllers/User/PaymentController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+     public function payment(Request $request)
+    {
+    	 /*$data=array();
+    	 $data['firstname']=$request->firstname;
+    	 $data['lastname']=$request->lastname;
+    	 $data['email']=$request->email;
+    	 $data['telephone']=$request->telephone;
+    	 $data['fax']=$request->fax;
+    	 $data['company']=$request->company;
+    	 $data['address_1']=$request->address_1;
+    	 $data['address_2']=$request->address_2;
+    	 $data['city']=$request->city;
+    	 $data['postcode']=$request->postcode;
+    	 $data['country_id']=$request->country_id;
+    	 $data['zone_id']=$request->zone_id;
+    	 $data['payment']=$request->payment;
+    	 $data['comments']=$request->comments;*/
+
+    	 /*return response()->json( $data);*/
+    	 /*dd($data);*/
+    	 echo "hfdkjs jdfskg";
+
+    	/* if ($request->payment == 'stripe') {
+
+    	 	  //stripe payment pages
+    	 	 return view('pages.payment.stripe',compact('data'));
+    	 
+    	 }elseif($request->payment == 'paypal'){
+
+    	 }elseif($request->payment == 'ideal'){
+
+    	 }else{
+    	 	echo"handcash";
+    	 }
+*/
+    }
+}
